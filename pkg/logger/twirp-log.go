68bb8234f3c9270258f1e3011becb539e8746e26
sgraham785 <sgraham785@gmail.com>
2018-09-10 16:46:58 -0700

    accepts message as first param

--- pkg/logger/twirp-log.go
+++ pkg/logger/twirp-log.go
@@ -21,7 +21,7 @@ type Logger struct {
 }
 
 // TwirpLog implemetents logging middleware
-func (l *Logger) TwirpLog(ctx context.Context, input, output interface{}, message string, err error, took time.Duration) error {
+func (l *Logger) TwirpLog(ctx context.Context, message string, input, output interface{}, err error, took time.Duration) error {
 	pkg, _ := twirp.PackageName(ctx)
 	service, _ := twirp.ServiceName(ctx)
 	method, _ := twirp.MethodName(ctx)
